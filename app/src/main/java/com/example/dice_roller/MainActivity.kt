ca3d847f5813edf686993926007859d6d87ae04e
rmchiavarino <rmchiavarino@gmail.com>
2023-08-10 10:56:40 -0300

    Modify: Reformat Code

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -6,27 +6,35 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * Esta Activity permite al usuario lanzar un dado y muestra el resultado
+ * en pantalla.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton:Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Lanza el dado y muestra el resultado en pantalla
+     */
     private fun rollDice() {
+        //Crea un objeto "dado" con 6 caras y genera un resultado
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView:TextView = findViewById(R.id.textView)
+
+        //Muestra en pantalla el resultado
+        val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice (private val numSides:Int){
-    fun roll():Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
