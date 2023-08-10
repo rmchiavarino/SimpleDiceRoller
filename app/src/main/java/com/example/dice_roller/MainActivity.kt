efcfbcdf2a2934f03d4d351248b0e2bf9e4db52a
rmchiavarino <rmchiavarino@gmail.com>
2023-08-10 10:43:17 -0300

    Add: fun rollDice

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -13,9 +13,15 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton:Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView:TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
 
